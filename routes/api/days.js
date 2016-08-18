c76e890f17fdb0afe5b9b4e3ab3cb6b915f7bd9e
Rachel Addleman <addlemanrachel@gmail.com>
2016-08-18 17:54:14 -0400

    Day routes Attempted get specific day, create new day and delete whole day.

--- routes/api/days.js
+++ routes/api/days.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Day = require ('../../models/day.js');
+var Day = require('../../models/day.js');
 module.exports = router;
 
 // Gets all the days
@@ -8,29 +8,40 @@ router.get('/days', function(req, res, next) {
     Day.findAll().then(function(days){
         res.json(days);
     })
-    .catch(next)
+    .catch(next);
 });
 
 // Gets specific day
 router.get('/days/:id', function(req, res, next) {
-    Day.findOne({where: 
-        {id: req.params.id}    
-    })
+    Day.findOne({where:
+        {id: req.params.id}
+    }).then(function(day){
+        res.json(day);
+ })
+ .catch(next);
 });
 
 // Create a new day
 router.post('/days', function(req, res, next) {
-    console.log('CREATE NEW DAY');
+    Day.findAll().then(function(days){
+        Day.create({
+            number: +days[days.length-1].number + 1
+        });
+    });
 });
 
 // Add attraction to a specific day
 router.post('/days/:id/:attractionType/:attractionId', function(req, res, next) {
-    console.log('ADD ATTRACTION TO DAY');
+    
 });
 
 // Delete a whole day
 router.delete('/days/:id', function(req, res, next) {
-    console.log('DELETE A DAY');
+   Day.findOne({where:
+    {id: req.params.id}   
+    }).then(function(day){
+        return day.destroy();
+    })
 });
 
 // Delete a particular attraction on a day
